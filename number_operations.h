487b0bec805a83346b11b85f64be624f4e9e59ba
Aksh Gupta <akshgpt7@gmail.com>
2021-05-16 23:33:09 +0530

    Add other functions

---
+++ number_operations.h
@@ -0,0 +1,10 @@
+/* The header file exposing all the functions */
+
+int sqroot(int num);
+int findsquare(int num);
+int paritycheck(int num);
+int twotopower(int num);
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
--- test.c
+++ test.c
@@ -1,12 +1,50 @@
 #include <stdio.h>
-#include "special_numbers.h"
+#include "number_operations.h"
 
 int main() {
-  int result;
-  int num = 2;
+  // initializing arguments to pass
+  int num = 16;
+  int a = 10; int b = 2;
 
-  result = findsquare(num);
-  printf("calling findsquare: Square of %i is %i\n", num, result);
+
+  int root = sqroot(num);
+  printf("calling sqroot: square root of %i is %i\n\n", num, root);
+
+
+  int sq = findsquare(num);
+  printf("calling findsquare: square of %i is %i\n\n", num, sq);
+
+
+  int parity = paritycheck(num);
+  if (parity == 0) {
+    printf("calling paritycheck: parity of %i is even\n\n", num);
+  }
+  else {
+    printf("calling paritycheck: parity of %i is odd\n\n", num);
+  }
+
+
+  int ans = twotopower(num);
+  printf("calling twotopower: 2 raised to the power %i is %i\n\n", num, ans);
+
+
+  int sum = add(a, b);
+  printf("calling add: sum of %i and %i is %i\n\n", a, b, sum);
+
+
+  int diff = sub(a, b);
+  printf("calling sub: difference of %i and %i is %i\n\n", a, b, diff);
+
+
+  int product = mul(a, b);
+  printf("calling mul: product of %i and %i is %i\n\n", a, b, product);
+
+
+  int quotient = div(a, b);
+  printf("calling div: %i divided by %i is %i\n\n", a, b, quotient);
+
   
   return 0;
+
+
 }
